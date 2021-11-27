495c7d5f2446b4649c5453f3783410c8b95df585
ZhangYinze <1285763952@qq.com>
2021-11-27 16:29:57 +0800

    feat: add WhileLoopsMustUseBracesRule and its test cases

---
+++ src/main/java/com/dylan/examples/Demo.java
@@ -0,0 +1,8 @@
+package com.dylan.examples;
+
+public class Demo {
+    public void doSomething() {
+        int x = 0;
+        while(true) x++;
+    }
+}
---
+++ src/test/java/com/dylan/WhileLoopsMustUseBracesRuleTest.java
@@ -0,0 +1,10 @@
+package com.dylan;
+
+import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+public class WhileLoopsMustUseBracesRuleTest extends SimpleAggregatorTst {
+    @Override
+    public void setUp() {
+        addRule("rulesets/rules.xml", "WhileLoopsMustUseBracesRule");
+    }
+}
